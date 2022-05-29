d493205848ecfe0412667641751b0dd00af25566
Azraar <mazraara@gmail.com>
2022-05-29 17:40:17 +0530

    Add missing SearchByName endpoint

---
+++ app/src/Controller/SearchByName.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\ContactsRepository;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+#[AsController]
+class SearchByName extends AbstractController
+{
+    protected ContactsRepository $contactRepository;
+
+    public function __construct(
+        ContactsRepository $contactRepository
+    ) {
+        $this->contactRepository = $contactRepository;
+    }
+
+
+    public function __invoke(Request $request): array
+    {
+        $keyword = $request->get('keyword');
+        if (!$keyword) {
+            throw new BadRequestHttpException('"keyword" is required');
+        }
+
+        $contacts = $this->contactRepository->findByName($request->get('keyword'));
+
+        if (!$contacts) {
+            throw $this->createNotFoundException(
+                'No record found...'
+            );
+        }
+
+        return $contacts;
+    }
+}
